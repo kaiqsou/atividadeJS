f54204b9a7ff0e0c14d39fe1ae43ed0276961add
Kaique <kaiqsouza2004@hotmail.com>
2025-02-20 18:49:17 -0300

    Resolução

---
+++ exercicio.js
@@ -0,0 +1,31 @@
+// Escreva uma função em JavaScript que recebe um array de números inteiros e retorna a soma dos números pares do array 
+
+const read = require('readline-sync');
+let numeros = [];
+let par = 0;
+
+
+// só mudar o limite do i para aumentar ou diminuir o array!
+for(let i = 0; i < 2; i++){
+    numeros[i] = parseInt(read.question("Digite o " + (i+1) + " numero: "));
+    if(numeros[i] % 2 === 0){
+        par += numeros[i];
+    } else if (Number.isInteger(numeros[i])) {
+        console.log("Numero não é inteiro!");
+        numeros[i] = 0;
+    } else {
+        console.log("Número inválido!");
+        numeros[i] = 0; 
+    }
+};
+
+if (par != 0){
+    for(let i = 0; i < 2; i++){
+        console.log("O " + (i+1) + " par é: " + numeros[i])
+    };
+        console.log("A soma dos pares é: " + par);
+} else {
+        console.log("Não há pares para somar.");
+}
+
+
